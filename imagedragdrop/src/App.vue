4fd06076879a9315c880e8a75ade886a710d6162
Krishna_chaithanya <krishna@immigrate.biz>
2021-11-20 19:36:09 -0500

    added images

--- imagedragdrop/src/App.vue
+++ imagedragdrop/src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <TabComponent />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TabComponent from "./components/tabComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TabComponent,
+  },
+  data() {
+    return {
+    };
+  },
+
+  methods: {},
+};
 </script>
 
 <style>
---
+++ imagedragdrop/src/components/tabComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-tabs v-model="tab" background-color="primary" dark>
+        <v-tab v-for="item in items" :key="item.tab">
+          {{ item.tab }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item v-for="item in items" :key="item.tab">
+          <v-card flat>
+            <v-card-text>
+              <component v-bind:is="item.content"></component>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import GroupA from "@/components/groupA.vue";
+import GroupB from "@/components/groupB.vue";
+import GroupC from "@/components/groupC.vue";
+
+export default {
+  name: "TabComponent",
+  components: {
+    GroupA,
+    GroupB,
+    GroupC,
+  },
+  data() {
+    return {
+      tab: null,
+      items: [
+        { tab: "Group_A", content: "GroupA" },
+        { tab: "Group_B", content: "GroupB" },
+        { tab: "Group_C", content: "GroupC" },
+      ],
+    };
+  },
+
+  methods: {},
+};
+</script>
+
+<style>
+</style>
